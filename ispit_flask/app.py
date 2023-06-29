108fac9c282df4b2bcd58721f38dfddec85807a0
Ivan Logožar <ivan.logozar@ecx.io>
2023-06-29 18:33:32 +0200

    test

--- ispit_flask/app.py
+++ ispit_flask/app.py
@@ -2,59 +2,61 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tutors.db"
 db = SQLAlchemy(app)
 
-# Define the Student model
-class Student(db.Model):
+
+class Tutor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     age = db.Column(db.Integer, nullable=False)
+    profession = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
         return self.name
 
-# Create some initial student records
+
 with app.app_context():
     db.create_all()
 
-    # Check if students table is empty
-    if Student.query.count() == 0:
-        # Add sample students
-        student1 = Student(name='John Doe', age=18)
-        student2 = Student(name='Jane Smith', age=17)
-        student3 = Student(name='David Johnson', age=19)
+    if Tutor.query.count() == 0:
+        tutor1 = Tutor(name="John Doe", age=30, profession="Math")
+        tutor2 = Tutor(name="Jane Smith", age=27, profession="Chemistry")
+        tutor3 = Tutor(name="David Johnson", age=32, profession="Biology")
 
-        # Add students to the session
-        db.session.add(student1)
-        db.session.add(student2)
-        db.session.add(student3)
+        db.session.add(tutor1)
+        db.session.add(tutor2)
+        db.session.add(tutor3)
 
-        # Commit the changes
         db.session.commit()
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/students')
-def students():
-    students = Student.query.all()
-    return render_template('students.html', students=students)
 
-@app.route('/request', methods=['GET', 'POST'])
+@app.route("/tutors")
+def tutors():
+    tutors = Tutor.query.all()
+    return render_template("tutors.html", tutors=tutors)
+
+
+@app.route("/request", methods=["GET", "POST"])
 def request_tutoring():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        age = request.form.get('age')
+    if request.method == "POST":
+        name = request.form.get("name")
+        age = request.form.get("age")
+        profession = request.form.get("profession")
 
-        student = Student(name=name, age=age)
-        db.session.add(student)
+        tutor = Tutor(name=name, age=age, profession=profession)
+        db.session.add(tutor)
         db.session.commit()
 
-        return render_template('request_success.html', name=name)
+        return render_template("request_success.html", name=name)
 
-    return render_template('request.html')
+    return render_template("request.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
